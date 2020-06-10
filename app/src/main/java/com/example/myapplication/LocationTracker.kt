a145d18ba43a4a89ff06c8da8cfcfae4923f02e2
chagayh <chagay.hof@gmail.com>
2020-06-10 13:23:04 +0300

    added curr location and saved bundle

--- app/src/main/java/com/example/myapplication/LocationTracker.kt
+++ app/src/main/java/com/example/myapplication/LocationTracker.kt
@@ -1,16 +1,55 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class LocationTracker (private var context: Context) {
-    private val LOG_TRACK = "TreackingLog"
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var locationInfo: LocationInfo = LocationInfo()
+    var isRecording: Boolean = false
+
+    private val LOG_TRACK = "trackingLog"
 
     fun startTracking() {
-        Log.d(LOG_TRACK, "start tracking")
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+            Log.d(LOG_TRACK, "start tracking")
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    locationInfo = LocationInfo(location?.accuracy,
+                                                location?.latitude,
+                                                location?.longitude)
+                    Log.d(LOG_TRACK, "accuracy = ${locationInfo.accuracy}, " +
+                            "latitude = ${locationInfo.latitude}," +
+                            "longitude = ${locationInfo.longitude}")
+                    // Got last known location. In some rare situations this can be null.
+                    // null -> gps off, device recorded problem,
+                    val intent = Intent("update_location")
+                    LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
+                }
+        }
+        else {
+            Log.d(LOG_TRACK, "Permission not granted")
+        }
     }
 
     fun stopTracking() {
         Log.d(LOG_TRACK, "stop tracking")
+    }
+
+    fun getLocationInfo(): LocationInfo{
+        return locationInfo
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,38 +1,84 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
 
     private val locationTracker = LocationTracker(this)
-    private var trackingBtn: Button? = null
-    private var setHomeBtn : Button? = null
-    private var textViewHomeLocation: TextView? = null
-    private var textViewCurrLocation: TextView? = null
-    private var appContext: GPSapp? = null
+    private val trackingBtn: Button
+            get() = findViewById(R.id.trackingBtn)
+    private val setHomeBtn : Button
+            get() = findViewById(R.id.setHomeBtn)
+    private val textViewHomeLocation: TextView
+            get() = findViewById(R.id.textViewHomeLocation)
+    private val textViewCurrLocation: TextView
+            get() = findViewById(R.id.textViewCurrLocation)
+    private val appContext: GPSapp
+            get() = applicationContext as GPSapp
+    private var locationInfo: LocationInfo = LocationInfo()
+    private val gson: Gson = Gson()
 
     private val REQUEST_CODE_PERMISSION_GPS = 1234
     private val TEXT_SET_HOME = "Set location as home"
     private val TEXT_DELETE_HOME = "Delete home location"
     private val TEXT_START_TRACKING = "Start Tracking"
     private val TEXT_STOP_TRACKING = "Stop Tracking"
+    private val LOG_PERMISSION = "permission"
+    private val KEY_TRACK_TEXT = "track_btn_text"
+    private val KEY_SET_HOME_TEXT = "set_home_btn_text"
+    private val KEY_HOME_LOCATION_VISIBILITY = "home_location_visibility"
+    private val KEY_IS_RECORDING = "is_recording"
+    private val KEY_LOCATION_INFO_OBJECT = "location_object"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        trackingBtn = findViewById(R.id.trackingBtn)
-        setHomeBtn  = findViewById(R.id.setHomeBtn)
-        textViewHomeLocation = findViewById(R.id.textViewHomeLocation)
-        textViewCurrLocation = findViewById(R.id.textViewCurrLocation)
-        appContext = applicationContext as GPSapp
-//        setButtons()
+        textViewHomeLocation.visibility = View.INVISIBLE
+        if (savedInstanceState != null) {
+            trackingBtn.text = savedInstanceState.getString(KEY_TRACK_TEXT)
+            setHomeBtn.text = savedInstanceState.getString(KEY_SET_HOME_TEXT)
+            textViewHomeLocation.visibility = savedInstanceState.getInt(KEY_HOME_LOCATION_VISIBILITY)
+            locationTracker.isRecording = savedInstanceState.getBoolean(KEY_IS_RECORDING)
+            val locationObjectAsJson: String? = savedInstanceState.getString(KEY_LOCATION_INFO_OBJECT)
+            if (locationObjectAsJson != null) {
+                val locationType = object : TypeToken<LocationInfo>(){}.type
+                locationInfo = gson.fromJson(locationObjectAsJson, locationType)
+            }
+            setLocationView()
+        }
+        setButtons()
+        val broadcastReceiver = (object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d("updateLocation", "updated location")
+                locationInfo = locationTracker.getLocationInfo()
+                setLocationView()
+            }
+        })
+        LocalBroadcastManager.getInstance(appContext)
+            .registerReceiver(broadcastReceiver, IntentFilter("update_location"))
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setLocationView(){
+        textViewCurrLocation.text = "Accuracy = ${locationInfo.accuracy}\n" +
+                "Latitude = ${locationInfo.latitude}\n" +
+                "Longitude = ${locationInfo.longitude}"
     }
 
     private fun setButtons(){
@@ -76,6 +122,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION_GPS -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        Log.d(LOG_PERMISSION, "Asked more than once")
+                        // reached here? means we asked the user for this permission more than once,
+                        // and they still refuse. This would be a good time to open up a dialog
+                        // explaining why we need this permission
+                    }
+                    Log.d(LOG_PERMISSION, "Permission has been denied by user")
+                } else {
+                    // TODO - retrieve data
+
+                    Log.d(LOG_PERMISSION, "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
     private fun isPermissionGranted(): Boolean{
         return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED)
@@ -86,6 +157,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_TRACK_TEXT, trackingBtn.text as String?)
+        outState.putString(KEY_SET_HOME_TEXT, setHomeBtn.text as String?)
+        outState.putInt(KEY_HOME_LOCATION_VISIBILITY, textViewHomeLocation.visibility)
+        outState.putBoolean(KEY_IS_RECORDING, locationTracker.isRecording)
+        val locationObjectAsJson = gson.toJson(locationInfo)
+        outState.putString(KEY_LOCATION_INFO_OBJECT, locationObjectAsJson)
+    }
+
     // TODO - save the last know location when exit the activity (flip the screen)
-    // TODO - broadcast
+    // TODO - unregister broadcast
+    // TODO - save the text of the buttons when flipping the phone
 }
